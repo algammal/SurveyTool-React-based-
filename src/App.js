c08eb56cea08c47fc6aae653797bb5cad7c4d777
algammal <algammal@eg.ibm.com>
2020-03-14 12:31:08 +0200

    fixed file name

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav'
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='col-xs-12 admin page-container no-padding'>
+        <div className='col-xs-12 menu-contaier no-padding'>
+            <Nav/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const Nav = () => {
+    return(
+        <div>
+            <a href='#'>Logo</a>
+            <ul className="col-xs-12 menu-allitems no-padding">
+                <a href='/'>Home</a>
+                <a href='/about'>About</a>
+            </ul>
+        </div>
+    )
+}
+export default Nav
