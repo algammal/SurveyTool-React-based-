c6944c5b2b89d4692ba17a52c519de26f9620489
algammal <algammal@eg.ibm.com>
2020-03-14 15:34:03 +0200

    removed unwanted data

--- src/components/BtnsComponent.js
+++ src/components/BtnsComponent.js
@@ -4,12 +4,7 @@ class BtnsComp extends Component {
     render(){
     return(
         <div className='newline'>
-            <button class="quest-itemplus btn btn-lg  glyphicon glyphicon-plus-sign">
-                <span class="add-txt">Add Question</span>
-            </button>
-            <button class="save-survey btn btn-lg  glyphicon glyphicon-floppy-disk">
-                <span class="add-txt">Save Survey</span>
-            </button>   
+            
         </div>
     )
     }
