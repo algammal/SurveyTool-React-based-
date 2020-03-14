20ef3a9d3e6962144686d39bab7cced6610f27e2
algammal <algammal@eg.ibm.com>
2020-03-14 22:02:05 +0200

    Fixed issues in component card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,29 @@
 import React, { Component } from 'react';
 
 class Card extends Component {
+    state={
+        items:[
+            { id:1 , name:'ahmed' , age:28 },
+            { id:2 , name:'ibrahim' , age:30 },
+            { id:3 , name:'mohamed' , age:40 }
+        ] 
+    }
+    handelClick = (e) => {
+        let items=this.state.items
+        items.push({id:4 ,name:'hussien',age:14} )
+        this.setState({
+            items :items
+        })
+    }
     render(){
     return(
         <div className='newline'>
+                <button onClick={this.handelClick} className="quest-itemplus btn btn-lg  glyphicon glyphicon-plus-sign">
+                <span className="add-txt">Add Question</span>
+            </button>
+            <button className="save-survey btn btn-lg  glyphicon glyphicon-floppy-disk">
+                <span className="add-txt">Save Survey</span>
+            </button> 
              <div className="example-1 content-added container" id="content">
                 <div className="example-2">
                     <div className="question-item col-xs-12">
@@ -15,14 +35,8 @@ class Card extends Component {
                         <div className="col-xs-12 mcq-answer">
                         <form>
                         <div className="check-answer">
-                            <input type="checkbox" id="ans1" name="subscribe" value="newsletter"/>
+                            <input type="checkbox" id="1" name="subscribe" value="newsletter"/>
                             <label className="chk-ans">red</label>
-                            <input className="chk-ans" type="checkbox" id="ans2" name="subscribe" value="newsletter"/>
-                            <label className="chk-ans" >blue</label>
-                            <input type="checkbox" id="ans3" name="subscribe" value="newsletter"/>
-                            <label className="chk-ans" >green</label>
-                            <input type="checkbox" id="ans4" name="subscribe" value="newsletter"/>
-                            <label className="chk-ans" >black</label>
                         </div>
                         </form>
                         </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,12 @@ import Card from './Card'
 
 
 class Home extends Component {
-    render(){
-        function repeat(e) {
-            e.preventDefault();    
-            return(
-               <Card/>
-            )
-        }
-        
+   
+    render(){ 
     return(
     <div className='newline'>
-             <div className='newline'>
-                <button onClick={repeat} className="quest-itemplus btn btn-lg  glyphicon glyphicon-plus-sign">
-                    <span className="add-txt">Add Question</span>
-                </button>
-                <button className="save-survey btn btn-lg  glyphicon glyphicon-floppy-disk">
-                    <span className="add-txt">Save Survey</span>
-                </button>   
-            </div>
+            
             <Card/>
-            {this.repeat}
         }
         </div>
     )
